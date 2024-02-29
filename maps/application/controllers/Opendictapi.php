902f0870213a86a88b8beca7b753ab62cdac9d80
skysongii <tjsgh9414@naver.com>
2024-02-29 09:57:20 +0900

    로딩바 구현 완료 및 소스 개선

--- maps/application/controllers/Opendictapi.php
+++ maps/application/controllers/Opendictapi.php
@@ -1,19 +1,30 @@
 <?php
-$search = $_POST['search'];
+$search = $_POST['word_arr'];
 
-$ch = curl_init();  //파싱 대상을 담을 $ch변수를 초기화
-$url = 'https://stdict.korean.go.kr/api/search.do'; /*URL*/
-$queryParams = '?' . 'key' . '=E6EE170EC92AB966F1CE9082E6BFEEC0'; /*Service Key*/
-$queryParams .= '&' . 'q' . '=' . urlencode($search); //요청인수
-$queryParams .= '&' . 'type_search' . '=' . 'search'; //요청인수
-$queryParams .= '&' . 'req_type=json';
+$json_de = json_decode($search, true);
+$result_arr = [];
 
-curl_setopt($ch, CURLOPT_URL, $url . $queryParams); //파싱 대상 설정
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);	//파싱한 결과를 string 형식으로 반환하도록 설정
+foreach($json_de as $key=>$value){
+    $search = $value['search'];
+//     echo $search;
 
-$response = curl_exec($ch);
-curl_close($ch);
+    $ch = curl_init();  //파싱 대상을 담을 $ch변수를 초기화
+    $url = 'https://stdict.korean.go.kr/api/search.do'; /*URL*/
+    $queryParams = '?' . 'key' . '=E6EE170EC92AB966F1CE9082E6BFEEC0'; /*Service Key*/
+    $queryParams .= '&' . 'q' . '=' . urlencode($search); //요청인수
+    $queryParams .= '&' . 'type_search' . '=' . 'search'; //요청인수
+    $queryParams .= '&' . 'req_type=json';
+    
 
-$json_response = json_encode($response,JSON_UNESCAPED_UNICODE);
+        curl_setopt($ch, CURLOPT_URL, $url . $queryParams); //파싱 대상 설정
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);	//파싱한 결과를 string 형식으로 반환하도록 설정
+        
+        $response = curl_exec($ch);
+        array_push($result_arr, $response);
+        curl_close($ch);
 
-echo $json_response;
+    // $json_response = json_encode($response,JSON_UNESCAPED_UNICODE);
+    
+}    
+echo json_encode($result_arr);
+
