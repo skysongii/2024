411ba86b8b3dc3fca0484a284569b829f557a05d
skysongii <tjsgh9414@naver.com>
2024-03-05 13:34:53 +0900

    표준시간대 변경 및 뽀요(openAPI) - ci 작업중

---
+++ ci/app/Controllers/Mysqltest.php
@@ -0,0 +1,16 @@
+<?php 
+namespace App\Controllers;
+use App\Controllers\BaseController;
+
+class Mysqltest extends BaseController
+{
+    public function index()
+    {
+        $apiModel = model('MysqltestModel');
+        $data = $apiModel->findAll();
+
+        var_dump($data);
+        // return view('Task/index', ['tasks' => $data]);
+    }
+
+}
